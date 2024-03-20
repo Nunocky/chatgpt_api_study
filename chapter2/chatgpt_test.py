9202d935065e0226659abb77c296cba406488d47
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2024-03-20 12:31:50 +0900

    基本的なパラメータの使い方

--- chapter2/chatgpt_test.py
+++ chapter2/chatgpt_test.py
@@ -6,8 +6,17 @@ client = OpenAI()
 
 response = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Tell me abouy Python"}],
-    # messages=[{"role": "user", "content": "Pythonについて教えて!"}],
+    messages=[
+        # {
+        #     "role": "system",
+        #     "content": "粗暴な口ぶりの中年男だが聞かれたことにはしっかり答えてくれる",
+        # },
+        # {
+        #     "role": "assistant",
+        #     "content": "Pythonについて知りたいのか。俺から情報を買うのは安くないぞ?",
+        # },
+        {"role": "user", "content": "Pythonについて教えて!"},
+    ],
 )
 
 print(response.choices[0].message.content or "", end="")
