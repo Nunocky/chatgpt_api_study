7c8aff317f8826863665f0516ce4926c473a1fcc
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2024-04-07 15:51:55 +0900

    不明事項をコメントとして追記

--- chapter6/app.py
+++ chapter6/app.py
@@ -1,39 +1,23 @@
 #!/usr/bin/env python3
 
-import pprint
-
-# from langchain.agents import initialize_agent
+from pprint import pprint
 from langchain.agents import Tool
 from langchain_community.utilities.google_search import GoogleSearchAPIWrapper
-from langchain.prompts import PromptTemplate
-from langchain.agents import AgentType
 from langchain_openai import ChatOpenAI
 from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 
 
-# LangChainDeprecationWarning: The function `initialize_agent` was deprecated in LangChain 0.1.0 and will be
-# removed in 0.2.0. Use Use new agent constructor methods like
-# create_react_agent, create_json_agent, create_structured_chat_agent, etc. instead.
-
-
-def create_prompt(user_input):
-    prompt = PromptTemplate(
-        input_variables=["theme"],
-        # user_input=user_input,
-        # agent_type=AgentType.CHAT,
-        # agent_name="ChatOpenAI",
-        # agent=ChatOpenAI,
-        # tool=Tool.OPENAI,
-        template="""
-        あなたはニュース記事を書くブロガーです。
-        下記のテーマについて、 google検索で最新情報を取得し、取得した情報に基づいてニュース記事を書いてください。
-        1000文字以上で、日本語で出力してください。
-        記事の末尾に参照元としてタイトルと URL を出力してください。
-        ### テーマ : {theme}
-        """,
-    )
-    return prompt.format(theme=user_input)
+def create_question(user_input):
+    return {
+        "input": f"""
+            あなたはニュース記事を書くブロガーです。
+            下記のテーマについて、 google検索で最新情報を取得し、取得した情報に基づいてニュース記事を書いてください。
+            1000文字以上で、日本語で出力してください。
+            記事の末尾に参照元としてタイトルと URL を出力してください。
+            ### テーマ : {user_input}
+            """
+    }
 
 
 def define_tools():
@@ -48,34 +32,58 @@ def define_tools():
 
 
 def write_response_to_file(response, filename):
-    pprint.pprint(response)
+    # pprint(response)
 
     with open(filename, "w", encoding="utf-8") as file:
         file.write(response["output"])
     print("出力が完了しました")
 
 
-def main():
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", max_tokens=2000)
+if __name__ == "__main__":
+    user_input = input("記事のテーマを入力してください: ")
+    question = create_question(user_input)
 
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, max_tokens=2000)
     tools = define_tools()
-
-    # agent = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS)
-
-    # theme = input("記事のテーマを入力してください: ")
-    # prompt = create_prompt(theme)
     prompt = hub.pull("hwchase17/openai-tools-agent")
+    # TODO PromptTemplate はもう使われないの?
+    pprint(prompt)
 
-    # response = agent.run(prompt)
-    model = ChatOpenAI()
-
-    agent = create_openai_tools_agent(model, tools, prompt)
+    agent = create_openai_tools_agent(llm, tools, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools)
-
-    response = agent_executor.invoke({"input": "what is (4.5*2.1)^2.2?"})
+    response = agent_executor.invoke(question)
 
     write_response_to_file(response, "output.txt")
 
-
-if __name__ == "__main__":
-    main()
+# hub.pull("hwchase17/openai-tools-agent") を使ったときの promptの中身
+# ChatPromptTemplate(
+#     input_variables=['agent_scratchpad', 'input'],
+#     input_types={'chat_history': typing.List[typing.Union[langchain_core.messages.ai.AIMessage, langchain_core.messages.human.HumanMessage, langchain_core.messages.chat.ChatMessage, langchain_core.messages.system.SystemMessage, langchain_core.messages.function.FunctionMessage, langchain_core.messages.tool.ToolMessage]],
+#                  'agent_scratchpad': typing.List[typing.Union[langchain_core.messages.ai.AIMessage, langchain_core.messages.human.HumanMessage, langchain_core.messages.chat.ChatMessage, langchain_core.messages.system.SystemMessage, langchain_core.messages.function.FunctionMessage, langchain_core.messages.tool.ToolMessage]]
+#     },
+#     metadata={
+#         'lc_hub_owner': 'hwchase17',
+#         'lc_hub_repo': 'openai-tools-agent',
+#         'lc_hub_commit_hash': 'c18672812789a3b9697656dd539edf0120285dcae36396d0b548ae42a4ed66f5'
+#     },
+#     messages=[
+#         SystemMessagePromptTemplate(
+#             prompt=PromptTemplate(
+#                 input_variables=[],
+#                 template='You are a helpful assistant')
+#             ),
+#         MessagesPlaceholder(
+#             variable_name='chat_history',
+#             optional=True
+#             ),
+#         HumanMessagePromptTemplate(
+#             prompt=PromptTemplate(
+#                 input_variables=['input'],
+#                 template='{input}'
+#                 )
+#             ),
+#         MessagesPlaceholder(
+#             variable_name='agent_scratchpad'
+#             )
+#     ]
+# )
